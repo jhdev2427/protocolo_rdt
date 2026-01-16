658b3e61b2599ebf54afad2dd3fc7f151d850391
Ismael Lucas <ismaellucas@acad.ifma.edu.br>
2026-01-16 15:12:28 -0300

    Atualiza implementação do cliente e servidor

--- cliente.py
+++ cliente.py
@@ -10,9 +10,13 @@ def gerar_checksum(entrada):
     checksum=checksum.hexdigest()
     return checksum
 
+def reenviar_mensagem(mensagem):
+    clientSocket.sendto(mensagem.encode(),(serverName, serverPort))
+
+
 def temporizador_cliente(mensagem):
     i=0
-    while i<4:
+    while i<5:
         clientSocket.settimeout(3)
         try:
             ack_server, serverAddress = clientSocket.recvfrom(2048)
@@ -27,9 +31,6 @@ def temporizador_cliente(mensagem):
             print(modifiedMessage.decode())
             break
         
-def reenviar_mensagem(mensagem):
-    clientSocket.sendto(mensagem.encode(),(serverName, serverPort))
-    #clientSocket.sendto(checksum.encode(),(serverName, serverPort))
 
     
 def enviar_mensagem(quantidade):
@@ -45,14 +46,34 @@ def enviar_mensagem(quantidade):
         temporizador_cliente(message)
         package_number=int(package_number)
 
+def enviar_mensagem_corrompida(quantidade):
+    package_number=0
+    while package_number<quantidade:
+        package_number+=1
+        package_number=str(package_number)
+        message = input("Insira a mensagem:")
+        message_corrompida= message + "Texto_externo_adicionado"
+        checksum_cliente=gerar_checksum(message_corrompida)
+        message=message+" "+package_number+" "+f'{checksum_cliente}'
+        clientSocket.sendto(message.encode(),(serverName, serverPort))
+        #clientSocket.sendto(checksum.encode(),(serverName, serverPort))
+        temporizador_cliente(message)
+        package_number=int(package_number)
+
 
 def iniciar_aplicação():
-    opcao=int(input("Digite o número correspondente a opção que deseja:\n1.Entrega normal\n2.Atraso intencional\n3.Corrupção de dados"))
-    if(opcao==1):
+    while True:
+        opcao=int(input("Reliable Data Transfer (RDT) \n\nDigite o número correspondente a opção que deseja:\n1.Entrega normal\n2.Corrupção de dados\n"))
         numero_de_pacotes=int(input("Quantos pacotes deseja enviar?\n"))
-        enviar_mensagem(numero_de_pacotes)
-    else if(opcao==2):
-
+        if opcao==1:
+            enviar_mensagem(numero_de_pacotes)
+            break
+        elif opcao==2:
+            enviar_mensagem_corrompida(numero_de_pacotes)
+            break
+        elif type(opcao)!=int and opcao>2:
+            print("Entrada inválida!")
+            continue
 
 
 iniciar_aplicação()
--- servidor.py
+++ servidor.py
@@ -1,9 +1,10 @@
 from socket import *
 import hashlib
+import time
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(("127.0.0.1", serverPort))
-print("Servidor em execução.")
+
 
 def gerar_checksum(entrada):
     checksum=hashlib.sha256()
@@ -11,43 +12,66 @@ def gerar_checksum(entrada):
     checksum=checksum.hexdigest()
     return checksum
 
-def temporizador_cliente(ack_cliente):
-    i=0
-    while i<4:
-        serverSocketSocket.settimeout(3)
-        try:
-            ack_cliente, clientAddress = clientSocket.recvfrom(2048)
-        
-        except timeout:
-            reenviar_mensagem(mensagem)
-            i+=1
-
-        else:
-            print(ack_cliente.decode())
-            break
-        
-while True:
-    message, clientAddress= serverSocket.recvfrom(2048)
-    #checksum, clientAddress= serverSocket.recvfrom(2048)
-    message=message.decode()
-    message=message.split()
-    checksum, package_number=message[len(message)-1],message[len(message)-2]
-    message.pop(len(message)-1)
-    message.pop(len(message)-1)
-    message=" ".join(message)
-    checksum_server=gerar_checksum(message)
-    print(checksum,checksum_server)
-    if(checksum==checksum_server):
-        print("Mensagem íntegra!")
-        print(f'Pacote recebido: {package_number}')
-        ack=f'Pacote recebido {package_number}'
-        serverSocket.sendto(ack.encode(), clientAddress)
-        modifiedMessage = message.upper()
-        print(modifiedMessage)
-        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-
-    #package_number,clientAddress2= serverSocket.recvfrom(2048)
+def execucao_normal():
+    print("Servidor em execução.")
+    while True:
+        message, clientAddress= serverSocket.recvfrom(2048)
+        message=message.decode()
+        message=message.split()
+        checksum, package_number=message[len(message)-1],message[len(message)-2]
+        message.pop(len(message)-1)
+        message.pop(len(message)-1)
+        message=" ".join(message)
+        print(f'Mensagem do cliente: {message}')
+        checksum_server=gerar_checksum(message)
+        if(checksum==checksum_server):
+            print("Mensagem íntegra!")
+            print(f'Pacote recebido: {package_number}')
+            ack=f'Pacote recebido no servidor:{package_number}'
+            serverSocket.sendto(ack.encode(), clientAddress)
+            modifiedMessage = message.upper()
+            print(f'Mensagem modificada:{modifiedMessage}')
+            serverSocket.sendto(modifiedMessage.encode(), clientAddress)
     
-    else:
-        print("Mensagem corrompida!")
+        else:
+            print("Mensagem corrompida!")
+
+def execucao_com_atraso():
+    print("Servidor em execução.")
+    while True:
+        message, clientAddress= serverSocket.recvfrom(2048)
+        message=message.decode()
+        message=message.split()
+        checksum, package_number=message[len(message)-1],message[len(message)-2]
+        message.pop(len(message)-1)
+        message.pop(len(message)-1)
+        message=" ".join(message)
+        print(f'Mensagem do cliente: {message}')
+        checksum_server=gerar_checksum(message)
+        if(checksum==checksum_server):
+            print("Mensagem íntegra!")
+            print(f'Pacote recebido: {package_number}')
+            for k in range(0,5):
+                print(k)
+                time.sleep(1)
+            ack=f'Pacote recebido no servidor: {package_number}'
+            serverSocket.sendto(ack.encode(), clientAddress)
+            modifiedMessage = message.upper()
+            print(f'Mensagem modificada:{modifiedMessage}')
+            serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    
+        else:
+            print("Mensagem corrompida!")
+
+
+def iniciar_servidor():
+    opcao=int(input("Reliable Data Transfer(RDT)\nServidor\nDigite o número correspondente à opção que deseja:\n1.Execução normal;\n2.Atraso intencional.\n "))
+    if opcao==1:
+        execucao_normal()
+    elif opcao==2:
+        execucao_com_atraso()
+
+iniciar_servidor()
+
+
     
