853c4852552a5e55eee91705e00762bafa115f51
Ismael Lucas <ismaellucas@acad.ifma.edu.br>
2026-01-16 12:52:39 -0300

    Atualiza implementação do cliente e servidor

--- cliente.py
+++ cliente.py
@@ -1,18 +1,60 @@
 from socket import*
+import hashlib
 serverName = "127.0.0.1"
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-package_number=0
-numero_de_pacotes=int(input("Quantos pacotes deseja enviar?"))
-while package_number<numero_de_pacotes:
-    package_number+=1
-    package_number=str(package_number)
-    message = input("Insira a mensagem:")
-    message=message+" "+package_number
-    clientSocket.sendto(message.encode(),(serverName, serverPort))
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    print(modifiedMessage.decode())
-    package_number=int(package_number)
 
+def gerar_checksum(entrada):
+    checksum=hashlib.sha256()
+    checksum.update(entrada.encode())
+    checksum=checksum.hexdigest()
+    return checksum
+
+def temporizador_cliente(mensagem):
+    i=0
+    while i<4:
+        clientSocket.settimeout(3)
+        try:
+            ack_server, serverAddress = clientSocket.recvfrom(2048)
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        
+        except timeout:
+            reenviar_mensagem(mensagem)
+            i+=1
+
+        else:
+            print(ack_server.decode())
+            print(modifiedMessage.decode())
+            break
+        
+def reenviar_mensagem(mensagem):
+    clientSocket.sendto(mensagem.encode(),(serverName, serverPort))
+    #clientSocket.sendto(checksum.encode(),(serverName, serverPort))
+
+    
+def enviar_mensagem(quantidade):
+    package_number=0
+    while package_number<quantidade:
+        package_number+=1
+        package_number=str(package_number)
+        message = input("Insira a mensagem:")
+        checksum_cliente=gerar_checksum(message)
+        message=message+" "+package_number+" "+f'{checksum_cliente}'
+        clientSocket.sendto(message.encode(),(serverName, serverPort))
+        #clientSocket.sendto(checksum.encode(),(serverName, serverPort))
+        temporizador_cliente(message)
+        package_number=int(package_number)
+
+
+def iniciar_aplicação():
+    opcao=int(input("Digite o número correspondente a opção que deseja:\n1.Entrega normal\n2.Atraso intencional\n3.Corrupção de dados"))
+    if(opcao==1):
+        numero_de_pacotes=int(input("Quantos pacotes deseja enviar?\n"))
+        enviar_mensagem(numero_de_pacotes)
+    else if(opcao==2):
+
+
+
+iniciar_aplicação()
 
 #clientSocket.close()
--- servidor.py
+++ servidor.py
@@ -1,22 +1,53 @@
 from socket import *
+import hashlib
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(("127.0.0.1", serverPort))
 print("Servidor em execução.")
+
+def gerar_checksum(entrada):
+    checksum=hashlib.sha256()
+    checksum.update(entrada.encode())
+    checksum=checksum.hexdigest()
+    return checksum
+
+def temporizador_cliente(ack_cliente):
+    i=0
+    while i<4:
+        serverSocketSocket.settimeout(3)
+        try:
+            ack_cliente, clientAddress = clientSocket.recvfrom(2048)
+        
+        except timeout:
+            reenviar_mensagem(mensagem)
+            i+=1
+
+        else:
+            print(ack_cliente.decode())
+            break
+        
 while True:
     message, clientAddress= serverSocket.recvfrom(2048)
+    #checksum, clientAddress= serverSocket.recvfrom(2048)
     message=message.decode()
-    if(len(message)!=0):
-        message=message.split()
-        package_number=message[len(message)-1]
-        message.pop(len(message)-1)
-        message=" ".join(message)
+    message=message.split()
+    checksum, package_number=message[len(message)-1],message[len(message)-2]
+    message.pop(len(message)-1)
+    message.pop(len(message)-1)
+    message=" ".join(message)
+    checksum_server=gerar_checksum(message)
+    print(checksum,checksum_server)
+    if(checksum==checksum_server):
+        print("Mensagem íntegra!")
         print(f'Pacote recebido: {package_number}')
-        print(message)
+        ack=f'Pacote recebido {package_number}'
+        serverSocket.sendto(ack.encode(), clientAddress)
         modifiedMessage = message.upper()
         print(modifiedMessage)
         serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+    #package_number,clientAddress2= serverSocket.recvfrom(2048)
     
     else:
-        print("Mensagem vazia!")
+        print("Mensagem corrompida!")
     
